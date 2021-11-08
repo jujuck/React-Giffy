7514bebc32a9dac9d13da6889f6ed6fca7a99f67
Julien ichard WCS <julien.y.richard@gmail.com>
2021-11-08 15:44:26 +0100

    Step 2, mise en place du container GifList

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import SelectedGif from './components/SelectedGif';
+import GifList from './components/GifList';
 
 function App() {
   return (
     <div className="App">
       <SelectedGif />
+      <GifList />
     </div>
   );
 }
---
+++ src/components/GifList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GifList = () => {
+  return (
+    <div>
+      <p>Ma liste de Gifs</p>
+    </div>
+  );
+};
+
+export default GifList;
