90c029ac2e74e7042864040e8f8752fb219d4e1c
Julien ichard WCS <julien.y.richard@gmail.com>
2021-11-08 15:46:48 +0100

    Step 3, ajout du composant Gif

---
+++ src/components/Gif.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Gif = () => {
+  return (
+    <div>
+      <p>Mon GIF</p>
+    </div>
+  );
+};
+
+export default Gif;
--- src/components/GifList.jsx
+++ src/components/GifList.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Gif from './Gif';
 
 const GifList = () => {
   return (
     <div>
       <p>Ma liste de Gifs</p>
+      <Gif />
     </div>
   );
 };
