7a786d92e364c34274bd76235f7419e6338ee790
Julien ichard WCS <julien.y.richard@gmail.com>
2021-11-08 15:58:00 +0100

    Step 8, mise en place du useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { SelectedGif, GifList } from './components';
 
 function App() {
+  const [gifs, setGifs] = useState([]);
+
+  useEffect(() => {
+    console.log('Use Effect in APP');
+    console.log(gifs);
+    setGifs([]);
+  }, []);
+
   return (
     <div className="App">
       <SelectedGif />
