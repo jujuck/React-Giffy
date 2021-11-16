467e241e717147b194250d1039ca63340f51db54
Julien ichard WCS <julien.y.richard@gmail.com>
2021-11-16 15:05:49 +0100

    réinitialisation du projet

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import env from 'react-dotenv';
-import './App.css';
-import { SelectedGif, GifList } from './components';
+import React from 'react';
 
 function App() {
-  const [gifs, setGifs] = useState([]);
-
-  useEffect(() => {
-    fetch(
-      `http://api.giphy.com/v1/gifs/search?api_key=${env.GIPHY_API_KEY}&q=cheeseburgers&limit=5`,
-      { method: 'GET' }
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        setGifs(data.data);
-        console.log(data.data);
-      });
-  }, []);
-
   return (
     <div className="App">
-      <h1 className="App-header">Giphy App</h1>
-      <div className="content">
-        <SelectedGif />
-        <GifList gifs={gifs} />
-      </div>
+      <h1>Giphy App</h1>
     </div>
   );
 }
--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-import Gif from './Gif';
-import GifList from './GifList';
-import SelectedGif from './SelectedGif';
-
-export { Gif, GifList, SelectedGif };
